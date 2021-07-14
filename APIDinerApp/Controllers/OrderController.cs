616be8daa89bb21366d1a3f165f92638d0c90586
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-15 01:57:22 +0300

    Added Order model validation for Post Action for api/order, used as attribute for <ActionResult> Post, added new order logic into ActionResult + return Ok value as anonymous object in json format.

---
+++ APIDinerApp/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDinerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public OrderController(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> Post(OrderModel order)
+        {
+            var food = await _foodData.GetFood();
+            order.Total = order.Quantity * food.Where(x => x.Id == order.FoodId).First().Price;
+
+            int id = await _orderData.CreateOrder(order);
+
+            return Ok(new { Id = id});
+        }
+    }
+}
